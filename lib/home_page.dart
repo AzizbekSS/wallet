1eb566b438804743ac1615738d651db1a2166428
hp <sultonovazizbek73@gmail.com>
2025-07-07 16:14:44 +0500

    date format

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:month_picker_dialog/month_picker_dialog.dart';
 
 import 'widgets/header.dart';
 
@@ -10,21 +11,58 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  DateTime date = DateTime.now();
+  final double sum = 123_456_666;
 
-final DateTime date=DateTime(2025,10,1);
-final double sum=123_456_666;
+  void showCalendar(BuildContext context) {
+    showMonthPicker(
+      context: context,
+      initialDate: DateTime.now(),
+      lastDate: DateTime.now(),
+      firstDate: DateTime(2020),
+    ).then((month) {
+      if (month != null) {
+        setState(() {
+          date = month;
+        });
+      }
+    });
+  }
+
+  void previousMonth() {
+    if(date.year==2020&&date.month==1){
+      return;
+    }
+    setState(() {
+      date = DateTime(date.year, date.month - 1, date.day );
+    });
+  }
+
+  void nextMonth() {
+       if(date.year==DateTime.now().year&&date.month==DateTime.now().month){
+      return;
+    }
+      setState(() {
+      date = DateTime(date.year, date.month + 1, date.day );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My Wallet'),
-      ),
+      appBar: AppBar(title: Text('My Wallet')),
       body: SizedBox(
         width: double.infinity,
-        child: Header(date: date, sum: sum),
+        child: Header(
+          date: date,
+          sum: sum,
+          onTap: () {
+            showCalendar(context);
+          },
+          previousMonth:previousMonth ,
+          nextMont: () =>nextMonth(),
+        ),
       ),
     );
   }
 }
-
--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,24 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:month_picker_dialog/month_picker_dialog.dart';
+import 'package:intl/intl.dart';
 
 class Header extends StatelessWidget {
   const Header({
     super.key,
     required this.date,
     required this.sum,
+    required this.onTap,
+    required this.previousMonth,
+    required this.nextMont,
   });
 
   final DateTime date;
   final double sum;
-
-
-  void showCalendar(BuildContext context){
-    showMonthPicker(context: context,
-    initialDate: DateTime.now(),
-    lastDate: DateTime.now(),
-    firstDate: DateTime(2020),
-    );
-  }
+  final void Function() onTap;
+  final void Function() previousMonth;
+  final void Function() nextMont;
 
   @override
   Widget build(BuildContext context) {
@@ -26,25 +23,38 @@ class Header extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        TextButton(onPressed: (){
-          showCalendar(context);
-        }, child: Text(date.toString(),style: TextStyle(color: Colors.black),),),
+        TextButton(
+          onPressed: () {
+            onTap();
+          },
+          child: Text(DateFormat("MMMM, yyyy").format(date), style: TextStyle(color: Colors.black)),
+        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back_ios_new_rounded,),),
+            IconButton(
+              onPressed: previousMonth,
+              icon: Icon(Icons.arrow_back_ios_new_rounded),
+            ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Text(sum.toString(),style: TextStyle(
-                  fontSize: 35,fontWeight: FontWeight.bold
-                ),),
-                Text("so'm",style: TextStyle(fontWeight: FontWeight.bold,height: 0.1),)
+                Text(
+                  sum.toString(),
+                  style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "so'm",
+                  style: TextStyle(fontWeight: FontWeight.bold, height: 0.1),
+                ),
               ],
             ),
-            IconButton(onPressed: (){}, icon: Icon(Icons.arrow_forward_ios_rounded,),),
+            IconButton(
+              onPressed:  nextMont,
+              icon: Icon(Icons.arrow_forward_ios_rounded),
+            ),
           ],
-        )
+        ),
       ],
     );
   }
